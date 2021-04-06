8a30c33fa99253e0c450e91991e68be82c6053cb
sibinovikr <r.sibinovik@gmail.com>
2021-04-06 14:36:59 +0200

    homework03

---
+++ SEDC.CSharpOop.Homeworks03/SEDC.CSharpOop.Homeworks03/SEDC.CSharpOop.Homework03.01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SEDC.CSharpOop.Homework03._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] myArray = new int[6];            
+            Console.WriteLine("Enter integer no.1: ");
+            myArray[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter integer no.2: ");
+            myArray[1] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter integer no.3: ");
+            myArray[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter integer no.4: ");
+            myArray[3] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter integer no.5: ");
+            myArray[4] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter integer no.6: ");
+            myArray[5] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Expected output: ");
+            for (int i = 0; i <= myArray.Length - 1; i++)
+            {
+                if (myArray[i] % 2 == 0)
+                {
+                    sum += myArray[i];
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+    
+}
---
+++ SEDC.CSharpOop.Homeworks03/SEDC.CSharpOop.Homeworks03/SEDC.CSharpOop.Homework03.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SEDC.CSharpOop.Homework03._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = { "Marko", "Darko", "Bojan", "Stojan", "Martin" };
+
+            Console.WriteLine("Enter student group: (there are 1 and 2)");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num == 1)
+            {
+                Console.WriteLine("The students in G1 are : ");
+                for (int i = 0; i <= studentsG1.Length - 1; i++)
+                {
+                    Console.WriteLine(studentsG1[i]);
+                }
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("The students in G2 are : ");
+                for (int i = 0; i <= studentsG2.Length - 1; i++)
+                {
+                    Console.WriteLine(studentsG2[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for the group number ");
+
+            }
+        }
+    }
+}
