1fe16c31631347564075be25a8ffc32ea0c665d4
sibinovikr <r.sibinovik@gmail.com>
2021-04-06 14:41:57 +0200

    Class02-Excercise07

---
+++ SEDC.CSharpOop.Homeworks03/SEDC.CSharpOop.Homeworks03/SEDC.CSHarpOop.Class_02.Exercise07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SEDC.CSHarpOop.Class_02.Exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number from 1 to 3: ");
+            string inputNumber = Console.ReadLine();
+
+            switch (inputNumber)
+            {
+                case "1":
+                    Console.WriteLine("You got a new car!");
+                    break;
+                case "2":
+                    Console.WriteLine("You got a new plane!");
+                    break;
+                case "3":
+                    Console.WriteLine("You got a new bike!");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid number!");
+                    break;
+            }
+        }
+    }
+}
