b91f1549ccf7b10793c57c7bd95f1045c0155362
sibinovikr <r.sibinovik@gmail.com>
2021-04-18 00:33:27 +0200

    Homework8

---
+++ SEDC.CSharpOop.Homework08/SEDC.CSharpOop.Class08.Exercise03/Classes/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SEDC.CSharpOop.Class08.Exercises.Classes
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Genre FavoriteMusicType { get; set; }
+        public List<Song> FavoriteSongs { get; set; }
+
+        public Person(string firstName, string lastName, int age, Genre favoritMusic)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            FavoriteMusicType = favoritMusic;
+            FavoriteSongs = new List<Song>();
+        }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
